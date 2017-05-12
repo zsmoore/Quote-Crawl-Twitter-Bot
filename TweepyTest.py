8fcb9fe11024a7222efffa119e8fa39b83b1337d
zsmoore <zsmoore@buffalo.edu>
2017-05-12 02:08:29 -0400

    grabbing random quotes

--- TweepyTest.py
+++ TweepyTest.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
-
+import sys
+import random
 
 def auth_setup():
     
@@ -17,15 +18,33 @@ def auth_setup():
 
     return auth
 
+def get_friends(api, user, depth):
+
+    if depth == 1:
+        return []
+    user = api.get_user(user)
+    ret = user.friends()
+    for user in user.friends():
+        ret += get_friends(api, user, depth +1)
+    return ret
+
+
+def get_tweet(inFile):
+
+    possible = []
+    for tweet in inFile.read().split("DELIM"):
+        possible.append(tweet.strip())
+
+    return random.choice(possible)
+
 def main():
 
     auth = auth_setup()
     api = tweepy.API(auth)
 
-    public_tweets = api.home_timeline()
-    for tweet in public_tweets:
-        print(tweet.text)
-
+    filename = sys.argv[1]
+    tweet = get_tweet(open(filename, "r"))
+    api.send_direct_message(user='TylerOrlando28', text=tweet)
 
 if __name__ == "__main__":
     main()
